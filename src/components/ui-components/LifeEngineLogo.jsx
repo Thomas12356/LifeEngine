6559bd68fe5aad0c12c033bdf7ebbc01c13972a5
Thomas Eardley <te215@kent.ac.uk>
2026-03-02 15:19:42 +0000

    feat(logo): add basic LifeEngineLogo component

---
+++ src/components/ui-components/LifeEngineLogo.jsx
@@ -0,0 +1,10 @@
+import { Text, chakra } from "@chakra-ui/react";
+
+export default function LifeEngineLogo() {
+    return(
+        <Text >
+            <chakra.span>Life</chakra.span>
+            <chakra.span>Engine</chakra.span>
+        </Text>
+    )
+}
