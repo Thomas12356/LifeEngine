f1d4d2fe08bb28d523a639c6b5dac3ccf01e4413
Thomas Eardley <te215@kent.ac.uk>
2026-03-04 20:48:57 +0000

    feat(theme): Initialise  theme. Added initial brand colours.

---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+import { createSystem, defaultConfig } from "@chakra-ui/react"
+import { defineConfig } from "@chakra-ui/react";
+
+const customConfig = defineConfig( {
+    theme:{
+        tokens: {
+            colors: {
+                brand:{
+                    blueLight: {value: "#39A3FF"},
+                    blueDark: {value: "#004A89"},
+                    gray: {value: "#9A9A9A"}
+                },
+            },
+        },
+    },
+})
+export const system = createSystem(defaultConfig, customConfig)
