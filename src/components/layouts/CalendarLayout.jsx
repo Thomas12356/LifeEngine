21406af97cd574889f372aba008d235ac47c715f
Dan Dixon <dandixontab119@gmail.com>
2026-03-04 22:42:48 +0000

    feat(calendar) Calendar layout and page files created.

---
+++ src/components/layouts/CalendarLayout.jsx
@@ -0,0 +1,11 @@
+import { VStack } from "@chakra-ui/react"
+
+export default function CalendarLayout() {
+    return (
+        <VStack>
+            <h1>Calendar Menu</h1>
+            <h1>Calendar Header</h1>
+            <h1>Calendar Body</h1>
+        </VStack>
+    )
+}
---
+++ src/components/pages/CalendarPage.jsx
@@ -0,0 +1,18 @@
+/*
+ * Home Page Component
+ *
+ * Renders the content of the home page
+ *
+ * @component
+ * @returns {JSX.Element} The Home page component
+ *
+ * Last updated: 21/02/2026
+*/
+
+import CalendarLayout from "@layouts/CalendarLayout";
+
+export default function CalendarPage() {
+    return (
+        <CalendarLayout />
+    )
+}
