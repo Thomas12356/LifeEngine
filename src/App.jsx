ea2ed460ff21787e10c95aff56531baffc4a0df7
Thomas Eardley <te215@kent.ac.uk>
2026-03-05 14:12:15 +0000

    feat(app): Background styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 //-----Imports-----//
-import { Box } from "@chakra-ui/react";
+import { Provider } from "@/components/ui/provider"
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Box } from "@chakra-ui/react"
+//Components
+import NavBar from "@layouts/NavBar"
 //Pages
 import Home from "@pages/Home"
 
 export default function App() {
     return (
-        <Box>
+        <Provider>
             <BrowserRouter>
+            <Box
+                position="fixed"
+                top="0"
+                left="0"
+                w="100vw"
+                h="100vh"
+                bg="radial-gradient(circle, white, #EDEDED)"
+                zIndex="-1"
+            >
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/calendar" element={<h1>Calendar</h1>} />
                 </Routes>
+            </Box>
             </BrowserRouter>
-        </Box>
+        </Provider>
     )
 }
