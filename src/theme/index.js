ae69add52ac431e636a5607e3819cb49390ab727
Thomas Eardley <te215@kent.ac.uk>
2026-03-06 11:50:28 +0000

    feat(theme): new colour grayLight

--- src/theme/index.js
+++ src/theme/index.js
@@ -29,6 +29,7 @@ const customConfig = defineConfig( {
                     blueLight: {value: "#39A3FF"},
                     blueDark: {value: "#004A89"},
                     gray: {value: "#9A9A9A"},
+                    grayLight: {value: "#F5F5F5"},
                 },
                 text: {
                     solid: { value: "#393939"},
@@ -52,14 +53,14 @@ const customConfig = defineConfig( {
             spacing: {
                 general: {
                     "xsSpacing": { value: "0.5rem" },
-                    "smSpacing": { value: "1.5rem" },
-                    "mdSpacing": { value: "2rem" },
-                    "lgSpacing": { value: "2.5rem" },
+                    "smSpacing": { value: "1rem" },
+                    "mdSpacing": { value: "1.5rem" },
+                    "lgSpacing": { value: "2rem" },
                 },
                 widget: {
                     "mLeftRight": { value: "1.75rem" },
                     "mTop": { value: "1.75rem" },
-                    "pLeftRight": { value: "1.5rem" },
+                    "pLeftRight": { value: "2rem" },
                     "pTopBottom": { value: "0.75rem" },
                 },
 
