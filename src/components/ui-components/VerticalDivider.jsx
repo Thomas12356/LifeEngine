9e885a985c3d1f446177f5a883e34fa079b805d8
Thomas Eardley <te215@kent.ac.uk>
2026-03-06 11:51:24 +0000

    feat(ui-components): new component for verticle dividers

---
+++ src/components/ui-components/VerticalDivider.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+export default function VerticalDivider() {
+    return(
+        <Box width="2px" bg="brand.grayLight" height="70%" marginLeft="general.smSpacing" marginRight="general.smSpacing"/>
+    )
+}
