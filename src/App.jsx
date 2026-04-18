ee0f1a66047ac5d00447c862c8042f57f34fd941
Thomas Eardley <te215@kent.ac.uk>
2026-04-18 18:44:01 +0100

    style(App): better readbility with background color change. Adding the Profile page to router. Adding spacing to a box container around contents of app.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { Box } from "@chakra-ui/react"
 //Components
 import NavBar from "@/features/navbar/NavBar"
 //Pages
-import Home from "@/pages/Home"
+import Home from "@pages/Home"
+import Profile from "@pages/Profile"
 
 export default function App() {
     return (
@@ -19,16 +20,20 @@ export default function App() {
                 left="0"
                 w="100vw"
                 h="100vh"
-                bg="radial-gradient(circle, white, #EDEDED)"
+                bg="radial-gradient(circle, #EDEDED, #EDEDED)"
                 zIndex="-1">
             </Box>
+
+            <Box pt={"widget.mTopBottom"} pb={"widget.mTopBottom"} pl={"widget.mLeftRight"} pr={"widget.mLeftRight"}>
 
                 <NavBar />
 
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/calendar" element={<h1>Calendar</h1>} />
+                    <Route path="/profile" element={<Profile/>}/>
                 </Routes>
+            </Box>
             
             </BrowserRouter>
 
