871f7ac63faf0ef8253b2132edeed51fbd2ad67c
Thomas Eardley <te215@kent.ac.uk>
2026-04-18 16:45:10 +0100

    wip(Pages): created the profile page.jsx file and initial fial content.

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+/**
+ * Profile Page Component
+ *
+ * Renders the content of the profile page
+ *
+ * @component
+ * @returns {JSX.Element} The profile page component
+ *
+ * Last updated: 18/04/2026 by te215@kent.ac.uk
+ */
+import { Box } from "@chakra-ui/react";
+
+export default function Profile(){
+    return(
+        <Box>
+            
+        </Box>
+    )
+}
