6d726825288cffeb5390ec779b975ad1c1b97db7
Thomas Eardley <te215@kent.ac.uk>
2026-04-28 15:23:27 +0100

    feat(scheduler-mvp): basic decision model scoring algorithm, considering adaptive waste cost + weight and net score calculation

--- backend/services/scheduler/SCHEDULER_MVP_LOG.md
+++ backend/services/scheduler/SCHEDULER_MVP_LOG.md
@@ -4,8 +4,12 @@
 **Purpose:** Schedule events based on a users metrics.
 **Current State:** Distance based scheduling. (v.0)
 
-## v.0 Initial Code
-- Pure Distance Based Scheduling
+## v.0 Basic Distance Based Scoring
+- Pure Distance Based Scoring only.
 
-## V.1 In Progress ...
+## V.1 Basic Decision Model Scoring
+- Basic Decision Model Scoring Only
+### Changes
+- Waste penalties have now been added, the consider the value of the time slot and have a weighted multiplier to lower their overall impact on the net score.
+- Calculate Net Score Function has now been added.
 
--- backend/services/scheduler/scheduler_mvp.py
+++ backend/services/scheduler/scheduler_mvp.py
@@ -1,4 +1,9 @@
+#V.1
 from dataclasses import dataclass
+
+#Config
+#Weights
+WASTE_COST_WEIGHT = 0.5
 
 @dataclass
 class Event:
@@ -12,8 +17,31 @@ class TimeSlot:
     predicted_energy: float # (low = 0, medium = 0.5, high = 1)
     predicted_focus: float # (low = 0, medium = 0.5, high = 1)
 
-#basic distance scoring function
-def score_event(event, timeslot):
-    energy_score = 1 - abs(event.ideal_energy - timeslot.predicted_energy)
-    focus_score = 1 - abs(event.ideal_focus - timeslot.predicted_focus)
-    return (energy_score + focus_score) / 2
+def score_fit(event, timeslot):
+    energy_fit = 1 - abs(event.ideal_energy - timeslot.predicted_energy)
+    focus_fit = 1 - abs(event.ideal_focus - timeslot.predicted_focus)
+    return (energy_fit + focus_fit) / 2
+
+def time_slot_value(timeslot):
+    return (timeslot.predicted_energy + timeslot.predicted_focus) / 2
+
+def waste_cost(event, timeslot):
+    return time_slot_value(timeslot) * (1 - event.ideal_energy) * WASTE_COST_WEIGHT
+
+def compute_net_score(event, timeslot):
+    fit_score = score_fit(event, timeslot) - waste_cost(event, timeslot)
+    cost = waste_cost(event, timeslot)
+    return fit_score - cost
+
+#Test
+def test():
+    event = Event("meeting", ideal_energy=0, ideal_focus=1)
+    timeslot = TimeSlot(hour=10, predicted_energy=1, predicted_focus=0.3)
+    
+    fit_score = score_fit(event, timeslot)
+    net_score = compute_net_score(event, timeslot)
+    
+    print(f"Fit Score: {fit_score}")
+    print(f"Net Score: {net_score}")
+
+test()
