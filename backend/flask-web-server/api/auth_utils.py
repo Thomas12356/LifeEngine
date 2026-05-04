02d11a9dc95b31a746cc1006f2513fabd54a44db
Thomas Eardley <te215@kent.ac.uk>
2026-05-04 18:19:04 +0100

    feat(auth): auth util functions for generating a salted and pepperd hash as well as verifying them

---
+++ backend/flask-web-server/api/auth_utils.py
@@ -0,0 +1,32 @@
+"""
+Util functions for authentication
+"""
+
+from hashlib import sha256
+from os import urandom
+
+# ! for production move to env variable.
+PEPPER = ""
+
+def generate_hash(password):
+    """
+    Generates a salted and peppered hash for a password.
+    """
+
+    salt = urandom(32).hex()
+
+    salted_peppered_password = password + salt + PEPPER
+    password_hash = sha256(salted_peppered_password.encode()).hexdigest()
+
+    return password_hash, salt
+
+
+def verify_password(password, stored_salt, stored_hash):
+    """
+    Verifies a password against its stored hash.
+    """
+    
+    salted_peppered_password = password + stored_salt + PEPPER
+    password_hash = sha256(salted_peppered_password.encode()).hexdigest()
+
+    return password_hash == stored_hash
