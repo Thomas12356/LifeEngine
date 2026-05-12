48ec65ff29bda461f77942d4f99effb6f8e21eeb
Thomas Eardley <te215@kent.ac.uk>
2026-05-12 14:47:15 +0100

    refactor(auth): improved structure and modularity of auth utils

--- backend/flask-web-server/app/auth_utils.py
+++ backend/flask-web-server/app/auth_utils.py
@@ -6,36 +6,53 @@ import os
 import hmac
 from dotenv import load_dotenv
 
+
 # Load environment variables from .env file
 load_dotenv()  
+
 
 # Load authentication config from .env
 PEPPER = os.environ.get('PEPPER')
 if not PEPPER:
     raise RuntimeError("[Error] PEPPER is not set in .env")
 
+
 PBKDf2_COMPLEXITY = int(os.environ.get('PBKDF2_COMPLEXITY', 600000))
 
 
-def generate_hash(password):
+def generate_salt():
     """
-    Generates a salted and peppered pbkdf2 hash for a password.
+    Generates a random salt.
     """
-    salt = os.urandom(32).hex()
+    return os.urandom(32).hex()
 
+
+def run_hashing_algorithm(password, salt):
+    """
+    Runs the pbkdf2 hashing algorithm on a password, salt and pepper.
+    """
     peppered_password = password + PEPPER
     
     password_hash = hashlib.pbkdf2_hmac('sha256', peppered_password.encode('utf-8'), salt.encode('utf-8'), PBKDf2_COMPLEXITY)
 
-    return password_hash.hex(), salt
+    return password_hash.hex()
 
 
-def verify_password(password, stored_salt, stored_hash):
+def generate_hash(password):
     """
-    Verifies a password against its stored pbkdf2 hash.
+    Generates a salted and peppered hash for a password.
     """
+    salt = generate_salt()
     
-    peppered_password = password + PEPPER
-    new_password_hash = hashlib.pbkdf2_hmac('sha256', peppered_password.encode('utf-8'), stored_salt.encode('utf-8'), PBKDf2_COMPLEXITY)
+    password_hash = run_hashing_algorithm(password, salt)
 
-    return hmac.compare_digest(new_password_hash.hex(), stored_hash)
+    return password_hash, salt
+
+
+def verify_password(new_password, stored_salt, stored_hash):
+    """
+    Verifies a password by hashing it and comparing against its stored hash.
+    """
+    new_password_hash = run_hashing_algorithm(new_password, stored_salt)
+
+    return hmac.compare_digest(new_password_hash, stored_hash)
