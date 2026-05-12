3d6db15749b3bcdcbcd01a0182b615ce8ee48b92
Thomas Eardley <te215@kent.ac.uk>
2026-05-12 19:55:32 +0100

    feat(react api): basic api intercept set up with axios for auth of access tokens

---
+++ frontend/src/api/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+    //backend URL
+    baseURL: '/api'
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
