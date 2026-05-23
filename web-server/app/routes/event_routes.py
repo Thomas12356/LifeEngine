a58ae8b923f5411057cb96acffc673bf52618572
Dan Dixon <djd41@kent.ac.uk>
2026-05-24 00:06:22 +0100

    wip(add event) : Implemented create_parameters route and service

--- web-server/app/routes/event_routes.py
+++ web-server/app/routes/event_routes.py
@@ -4,11 +4,15 @@ from app.services import event_services
 event_blueprint = Blueprint('event',__name__)
 
 REQUIRED_EVENT_FIELDS = ['user_id', 'name', 'start_time', 'end_time']
+REQUIRED_EVENT_PARAMETER_FIELDS = ["ideal_energy", "burnout_rate", "priority"]
 
 @event_blueprint.route('/addevent', methods=['POST'])
 def add_event():
     data = request.get_json()
 
+    # DEBUG - Remove later
+    print(data)
+    
     # check required data has been given.
     for field in REQUIRED_EVENT_FIELDS:
         if field not in data or not data[field]:
@@ -31,4 +35,27 @@ def add_event():
     return jsonify({
         "message": "Event created successfully", 
         "event_id": result["event_id"]
+    }), 201
+
+@event_blueprint.route("/createparameters", methods=['POST'])
+def create_parameters():
+
+    data = request.get_json()
+
+    for field in REQUIRED_EVENT_PARAMETER_FIELDS:
+        if field not in data or not data[field]:
+            return jsonify({"error": f"Missing required field: {field}"}), 400
+
+    result = event_services.create_event_parameters(
+        ideal_energy=float(data["ideal_energy"]),
+        burnout_rate=float(data["burnout_rate"]),
+        priority=int(data["priority"])
+    )
+
+    if not result["success"]:
+        return jsonify({"error": result["error"]}), result["status_code"]
+
+    return jsonify({
+        "message": "Event parameters created successfully", 
+        "event_parameters_id": result["event_parameters_id"]
     }), 201
--- web-server/app/services/event_services.py
+++ web-server/app/services/event_services.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 from app import db
-from app.models import Event
+from app.models import Event, EventParameter
 
 
 def create_event(user_id_str: str, name, start_time_str: str, end_time_str: str, event_type_id_str: str = None, event_parameter_id_str: str = None, is_moveable: bool = False, is_active: bool = True):
@@ -39,4 +39,43 @@ def create_event(user_id_str: str, name, start_time_str: str, end_time_str: str,
     except Exception as e:
         db.session.rollback()
         return {"success": False, "error": "Internal database error.", "status_code": 500}
+
+def create_event_parameters(ideal_energy : float, burnout_rate : float, priority: int):
+    """
+    Creates a record of event parameters, validates data and saves to db.
+    """
+    try:
+
+        if priority > 10 or priority < 1:
+            return {"success": False, "error": "priority must be between 1 and 10", "status_code": 400}
+        
+        if burnout_rate < 0:
+            return {"success": False, "error": "burnout_rate must be > 0", "status_code": 400}
+        
+        if ideal_energy < 0 or ideal_energy > 1:
+            return {"success": False, "error": "ideal_energy must be between 0 and 1", "status_code": 400}
+        
+        created_at = datetime.now()
+
+        new_parameters =  EventParameter(
+            ideal_energy=ideal_energy,
+            burnout_rate=burnout_rate,
+            priority=priority,
+            created_at=created_at
+        )
+
+        db.session.add(new_parameters)
+        db.session.commit()
+
+        return {"success": True, "event_parameters_id": str(new_parameters.id)}
+    
+    except ValueError as e:
+        return {"success": False, "error": f"Invalid data format: {str(e)}", "status_code": 400}
+    
+    except Exception as e:
+        db.session.rollback()
+        return {"success": False, "error": f"Internal database error. {str(e)}", "status_code": 500}
+
+
+
         
