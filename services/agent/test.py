ca9174c0dda502ed427a15564e2ce933e433fd1d
Thomas eardley <te215@kent.ac.uk>
2026-05-30 18:25:54 +0100

    wip(agent): test file

---
+++ services/agent/test.py
@@ -0,0 +1,13 @@
+from agent import *
+
+if __name__ == "__main__":
+    print("Agent test")
+
+    while True:
+        user_input = input("Enter Message: ")
+
+        try:
+            result = handle_message_from_user(user_input)
+            print(result)
+        except Exception as e:
+            print("Error: ", e)
