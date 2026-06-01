2feada32e7e7bd6f8a35b97dacc6ddfe9cbb0c16
djd41@kent.ac.uk <dandixontab119@gmail.com>
2026-06-01 03:15:34 +0100

    feat(auth_services) : added default eventtype creation on registration

--- web-server/app/services/auth_services.py
+++ web-server/app/services/auth_services.py
@@ -1,6 +1,10 @@
 from app import db
-from app.models import User, UserPreferences
+from app.models import User, UserPreferences, EventType
 from app.services.utils.auth_util import verify_password, generate_new_hash
+from app.services.event_parameter_services import create_event_parameters
+
+import uuid, os
+from datetime import datetime
 
 
 def register_user(email, password, first_name, last_name, wake_up, bed_time):
@@ -22,19 +26,42 @@ def register_user(email, password, first_name, last_name, wake_up, bed_time):
         last_name=last_name
     )
 
-    db.session.add(new_user)
+    db.session.add(new_user) # Add the new user to the database session and commit
     db.session.commit()
 
-    new_user_preferences = UserPreferences(
+    new_user_preferences = UserPreferences( # Build preferences
         user_id = new_user.id,
         wakeup_time = wake_up,
         bed_time = bed_time,
     )
-
-
-    # Add the new user to the database session and commit
     
-    db.session.add(new_user_preferences)
+    db.session.add(new_user_preferences) # Commit new preferences
+    db.session.commit()
+
+    defaults = { # Build default event type parameters
+        "ideal_energy": os.environ.get("DEFAULT_IDEAL_ENERGY"),
+        "burnout_rate": os.environ.get("DEFAULT_BURNOUT_RATE"),
+        "priority": os.environ.get("DEFAULT_PRIORITY"),
+    }
+
+    result = create_event_parameters(defaults) # Create default parameters
+
+    parameters_uuid = uuid.UUID(result["event_parameters_id"])
+
+    default = EventType(
+            user_id = new_user.id,
+            event_parameter_id = parameters_uuid,
+            name = "Default",
+            created_at = datetime.now(),
+            colour = os.environ.get("DEFAULT_COLOUR"),
+            availability_start = wake_up,
+            availability_end = bed_time,
+            preference_start = wake_up,
+            preference_end = bed_time
+
+        )
+    
+    db.session.add(default)
     db.session.commit()
 
     return new_user, None
