7e7e7cdef4eaf114b003d2978b73521be187a6f3
djd41@kent.ac.uk <dandixontab119@gmail.com>
2026-06-01 18:59:48 +0100

    wip(schedule_optimiser) : created input DTOs, event mapping function and auto reschedule task

---
+++ web-server/app/services/schedule_optimiser/dto/mapper.py
@@ -0,0 +1,63 @@
+"""
+    This file holds mapping functions for converting input DTOs to GA models
+"""
+
+from .input_dto import dbEventInput, dbEventTypeInput, dbUserPreferenceInput
+from ..models import Event, EventType
+from ..config import SLOT_SIZE
+from datetime import datetime
+
+# Given an ISO string convert to a slot index
+def convert_time(time : datetime):
+    return (time.hour * 60 + time.minute) // SLOT_SIZE
+
+# Given a slot index convert to HH:MM
+def convert_slot_index(slot_index):
+    total_minutes = slot_index * SLOT_SIZE
+    hours, minutes = divmod(total_minutes, 60)
+    return f"{hours:02d}:{minutes:02d}"
+
+def map_event(event_dto : dbEventInput, event_type_dto : dbEventTypeInput):
+    """
+        Given event, event type DTOs, map their values to Event and EventType
+    """
+    ideal_energy = event_dto.ideal_energy if event_dto.ideal_energy is not None else event_type_dto.ideal_energy
+    burnout_rate = event_dto.burnout_rate if event_dto.burnout_rate is not None else event_type_dto.burnout_rate
+    priority = event_dto.priority if event_dto.priority else event_type_dto.priority
+    is_moveable = event_dto.is_moveable if event_dto.is_moveable else event_type_dto.is_moveable
+
+    scheduler_event_type = EventType(
+        name=event_type_dto.name,
+        ideal_energy=ideal_energy,
+        ideal_focus=0, # TODO : Remove focus fields in future refactor
+        energy_weight=1, # NOTE : Unused but maybe required, review
+        focus_weight=1, # NOTE : Unused but maybe required, review
+        impact=0.2, # NOTE : DEFAULT VALUE FOR NOW, WE SHOULD TEST THIS
+        burnout_rate=burnout_rate,
+
+        availability_window=(
+            convert_time(event_type_dto.availability_start),
+            convert_time(event_type_dto.availability_end)
+        ),
+
+        preferred_window=(
+            convert_time(event_type_dto.preference_start),
+            convert_time(event_type_dto.preference_end)
+        )
+    )
+
+    scheduler_event = Event(
+        event_id=event_dto.id,
+        name=event_dto.name,
+        EventType=scheduler_event_type,
+        is_moveable=is_moveable,
+        importance=priority,
+
+        start_slot=convert_time(event_dto.start_time),
+        duration_slots=convert_time(event_dto.end_time) - convert_time(event_dto.start_time)
+
+    )
+
+    return scheduler_event
+
+
---
+++ web-server/app/services/schedule_optimiser/tasks/auto_reschedule.py
@@ -0,0 +1,19 @@
+"""
+
+    Scheduler task : Auto reschedule single event on the same day
+
+    Notes : 
+        - Reject immedietly if no time left in the day to fit in event
+        - Take in DTOS, they are built in the service file
+"""
+
+from app.services import event_services, event_type_services, event_parameter_services, user_services
+from app.models import Event, EventType, EventParameter, UserPreferences
+from ..dto.input_dto import dbEventInput, dbEventTypeInput, dbUserPreferenceInput
+from ..dto.mapper import map_event
+
+def auto_reschedule(event_to_reschedule : dbEventInput, event_type : dbEventTypeInput, user_preferences : dbUserPreferenceInput):
+    
+    print(map_event(event_dto=event_to_reschedule, event_type_dto=event_type))
+
+    
